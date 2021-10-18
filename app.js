d3d96001e46390ca9712d918af0cbd4daf62de7e
yujinpan <1192878390@qq.com>
2021-10-18 18:11:29 +0800

    feat: 增加代理配置

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var history = require('connect-history-api-fallback');
+// var history = require('connect-history-api-fallback');
+var createProxyMiddleware = require('http-proxy-middleware').createProxyMiddleware;
+var cors = require('cors');
+
+var proxyConfig = require('./proxy.config');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-app.use(history()); // use html5 history mode
+// app.use(history()); // use html5 history mode
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// proxy
+for(var key in proxyConfig) {
+  console.log(key);
+  app.use(key, createProxyMiddleware(proxyConfig[key]));
+}
 
 app.use('/', indexRouter);
 
---
+++ proxy.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '/arcgis': {
+    target: 'http://124.70.89.253:6080',
+    changeOrigin: true
+  }
+}
